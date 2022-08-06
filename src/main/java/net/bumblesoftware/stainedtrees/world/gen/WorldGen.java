c86708e4e1abc3ce279d732db7db555328d48542
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-08-07 02:32:51 +1000

    Update WorldGen.java

--- src/main/java/net/bumblesoftware/stainedtrees/world/gen/WorldGen.java
+++ src/main/java/net/bumblesoftware/stainedtrees/world/gen/WorldGen.java
@@ -1,21 +1,21 @@
 package net.bumblesoftware.stainedtrees.world.gen;
 
-public class WorldGen {
+public class STWorldGen {
     public static void generateWorldGen() {
-        TreeGeneration.generateBlackTrees();
-        TreeGeneration.generateBlueTrees();
-        TreeGeneration.generateBrownTrees();
-        TreeGeneration.generateCyanTrees();
-        TreeGeneration.generateGrayTrees();
-        TreeGeneration.generateLightBlueTrees();
-        TreeGeneration.generateLightGrayTrees();
-        TreeGeneration.generateLimeTrees();
-        TreeGeneration.generateMagentaTrees();
-        TreeGeneration.generateOrangeTrees();
-        TreeGeneration.generatePinkTrees();
-        TreeGeneration.generatePurpleTrees();
-        TreeGeneration.generateRedTrees();
-        TreeGeneration.generateWhiteTrees();
-        TreeGeneration.generateYellowTrees();
+        STTreeGeneration.generateBlackTrees();
+        STTreeGeneration.generateBlueTrees();
+        STTreeGeneration.generateBrownTrees();
+        STTreeGeneration.generateCyanTrees();
+        STTreeGeneration.generateGrayTrees();
+        STTreeGeneration.generateLightBlueTrees();
+        STTreeGeneration.generateLightGrayTrees();
+        STTreeGeneration.generateLimeTrees();
+        STTreeGeneration.generateMagentaTrees();
+        STTreeGeneration.generateOrangeTrees();
+        STTreeGeneration.generatePinkTrees();
+        STTreeGeneration.generatePurpleTrees();
+        STTreeGeneration.generateRedTrees();
+        STTreeGeneration.generateWhiteTrees();
+        STTreeGeneration.generateYellowTrees();
     }
 }
