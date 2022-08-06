f9f26fdcc3e3b98259a751411faefa4192a4d25a
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-08-07 02:31:59 +1000

    Update and rename PlacedFeatures.java to STPlacedFeatures.java

--- src/main/java/net/bumblesoftware/stainedtrees/world/feature/STPlacedFeatures.java
+++ src/main/java/net/bumblesoftware/stainedtrees/world/feature/STPlacedFeatures.java
@@ -4,62 +4,62 @@ import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.feature.VegetationPlacedFeatures;
 
-public class PlacedFeatures {
+public class STPlacedFeatures {
     public static final RegistryEntry<PlacedFeature> BLACK_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("black_oak_placed",
-            ConfiguredFeatures.BLACK_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.BLACK_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
     public static final RegistryEntry<PlacedFeature> BLUE_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("blue_oak_placed",
-            ConfiguredFeatures.BLUE_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.BLUE_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
     public static final RegistryEntry<PlacedFeature> BROWN_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("brown_oak_placed",
-            ConfiguredFeatures.BROWN_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.BROWN_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> CYAN_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("cyan_oak_placed",
-            ConfiguredFeatures.CYAN_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.CYAN_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> GRAY_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("gray_oak_placed",
-            ConfiguredFeatures.GRAY_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.GRAY_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> LIGHT_BLUE_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("light_blue_oak_placed",
-            ConfiguredFeatures.LIGHT_BLUE_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.LIGHT_BLUE_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> LIGHT_GRAY_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("light_gray_oak_placed",
-            ConfiguredFeatures.LIGHT_GRAY_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.LIGHT_GRAY_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> LIME_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("lime_oak_placed",
-            ConfiguredFeatures.LIME_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.LIME_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> MAGENTA_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("magenta_oak_placed",
-            ConfiguredFeatures.MAGENTA_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.MAGENTA_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> ORANGE_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("orange_oak_placed",
-            ConfiguredFeatures.ORANGE_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.ORANGE_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> PINK_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("pink_oak_placed",
-            ConfiguredFeatures.PINK_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.PINK_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> PURPLE_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("purple_oak_placed",
-            ConfiguredFeatures.PURPLE_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.PURPLE_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> RED_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("red_oak_placed",
-            ConfiguredFeatures.RED_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.RED_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> WHITE_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("white_oak_placed",
-            ConfiguredFeatures.WHITE_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.WHITE_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 
     public static final RegistryEntry<PlacedFeature> YELLOW_OAK_PLACED = net.minecraft.world.gen.feature.PlacedFeatures.register("yellow_oak_placed",
-            ConfiguredFeatures.YELLOW_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
+            STConfiguredFeatures.YELLOW_OAK_SPAWN, VegetationPlacedFeatures.modifiers(
                     net.minecraft.world.gen.feature.PlacedFeatures.createCountExtraModifier(0, 0.005f, 1)));
 }
