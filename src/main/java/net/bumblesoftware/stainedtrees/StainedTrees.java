d0575ed08c42306a9140cebea7c2c970eff66977
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-08-07 02:39:30 +1000

    mega Oops

--- src/main/java/net/bumblesoftware/stainedtrees/StainedTrees.java
+++ src/main/java/net/bumblesoftware/stainedtrees/StainedTrees.java
@@ -2,14 +2,14 @@ package net.bumblesoftware.stainedtrees;
 
 import net.bumblesoftware.stainedtrees.block.StainedTreeBlocks;
 import net.bumblesoftware.stainedtrees.util.BlockRegistry;
-import net.bumblesoftware.stainedtrees.world.gen.WorldGen;
+import net.bumblesoftware.stainedtrees.world.gen.STWorldGen;
 import net.fabricmc.api.ModInitializer;
 
 public class StainedTrees implements ModInitializer {
     public static String MOD_ID = "stained_trees";
     @Override
     public void onInitialize() {
-        WorldGen.generateWorldGen();
+        STWorldGen.generateWorldGen();
         StainedTreeBlocks.RegisterBlocks();
         BlockRegistry.registerFlammableBlock();
     }
